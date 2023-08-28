1a4d6322a6aed2f137ee6fa0b15c1e2d004552a6
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-08-28 11:37:09 -0600

    created post model

---
+++ backend/models/postModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    // not sure if this is the right data type,
+    // but i want to be able to display the information about the user that created the post
+    createdBy: { type: mongoose.Types.ObjectId },
+    teaser: { 
+        type: String,
+        required: true
+    },
+    spoiler: {
+        type: String,
+        required: true
+    },
+    trick: {
+        type: Boolean,
+        required: true
+    },
+    candyPoints: {
+        type: Number,
+        required: true
+    },
+    // not sure the best way to store likes and dislikes but i will start with just numbers
+    likes: { type: Number, default: 0 },
+    dislikes: {type: Number, default: 0}
+
+}, { timestamps: true })
+
+const Post = mongoose.model('posts', postSchema)
+
+module.exports = Posts
