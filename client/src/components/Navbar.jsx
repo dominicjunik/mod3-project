2089c479f4f594e38aef1ea9123bce12919bba0f
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-08-29 13:38:22 -0600

    added logout to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ username, setUser }) {
   // simple logout function, clears the state variable and deletes the token => this causes all the routes to change
+  function logout() {
+    localStorage.removeItem("token");
+    setUser({});
+  }
 
   return (
     <nav className="w-screen bg-slate-700">
@@ -10,7 +14,14 @@ export default function Navbar({ username, setUser }) {
           <Link to="/">Home</Link>
         </li>
         {username ? (
-          <></>
+          <>
+            <li>Welcome {username}</li>
+            <li>
+              <li onClick={logout}>
+                <Link to="/posts">Logout</Link>
+              </li>
+            </li>
+          </>
         ) : (
           <>
             <li>
