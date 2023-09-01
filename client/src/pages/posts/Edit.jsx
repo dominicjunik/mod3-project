f332a000cadf31e032036318acfe0fb6a7ff0d8d
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-09-01 09:40:43 -0600

    fixed the text wrapping issue with a min-width on a container inside another flex blox container

--- client/src/pages/posts/Edit.jsx
+++ client/src/pages/posts/Edit.jsx
@@ -1,7 +1,10 @@
 export default function Edit({username}){
     return(
-        <div>
-            Edit
+        <div className="flex flex-wrap items-center justify-center bg-slate-700 w-20 h-20 rounded-full m-2 text-center">
+            <p className="min-w-0 break-words">
+            reallyreallyreallylongname 
+            </p>
+              
         </div>
     )
 }
